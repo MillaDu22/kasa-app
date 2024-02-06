aa88ae32466c79bcb271226dbec077a2dc9186b5
Milla22 <marti.ludmilla@orange.fr>
2024-02-06 17:13:21 +0100

    purge squelette app, creation des dossiers et fichiers pages et du router, import d'images

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { HashRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+import Home from './pages/Home/index.jsx';
+import AProposPage from './pages/AProposPage/index.jsx';
+import LogementPage from './pages/LogementPage/index.jsx';
+import Error from './pages/Error/index.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/a-propos-page" element={< AProposPage/>} />
+                <Route path="/logement-page/:id" element={<LogementPage />} />
+                <Route path="*" element={<Error />} /> 
+                <Route path= "/" element={<Navigate replace to = "/" />} />
+            </Routes>
+    </HashRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
